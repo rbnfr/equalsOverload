6889fec1c608494c075f557627a4e7df74e8064a
Ruben_fr <ruben.f.alcaraz@gmail.com>
2017-08-02 00:07:27 +0200

    Actualización

--- equalsOverload/Program.cs
+++ equalsOverload/Program.cs
@@ -4,12 +4,28 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace equalsOverload
+namespace ConsoleApp2
 {
     class Program
     {
         static void Main(string[] args)
-        {
+        {     
+            var persons = new List<Person>() {
+                new Person(){Name="Pedro"},
+                new Person(){Name="Juan"},
+                new Person(){Name="Fernando"},
+            };
+
+            var Pedro = new Person() { Name = "Pedro" };
+            var Juan = new Person() { Name = "Pedro" };
+            var hermanos = Pedro + Juan;
+
+            var codePedro = Pedro.GetHashCode();//Obtener el número de hash
+            var codeJuan = Juan.GetHashCode();
+            
+            var iguales = Pedro.Equals(Juan);// Devuelve false, porque no está comparando la propiedad name --> sobrecarga operador --> devuelve true si los nombres son iguales
+            Console.WriteLine(iguales);
+            Console.ReadLine();
         }
     }
 }
